e32dfc83799019e4c987ba2762b57f4ca591b36d
dpf <1005244620@qq.com>
2019-12-17 19:14:26 +0800

    dev

--- src/main/java/com/dpf/App.java
+++ src/main/java/com/dpf/App.java
@@ -8,5 +8,6 @@ public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class);
         System.out.println("");
+        System.out.println("dev");
     }
 }
