3b5876c1030fa1ba6458252ad12b09ea2ad89a11
dpf <1005244620@qq.com>
2019-12-17 19:32:57 +0800

    ccc

--- src/main/java/com/dpf/App.java
+++ src/main/java/com/dpf/App.java
@@ -7,5 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class);
+        System.out.println("");
+        System.out.println("dev");
+        System.out.println("ccc");
     }
 }
